879cc67f6555ccf3e7ab1708635c967f8999295a
Cesar Aguilera <cesargnu29@gmail.com>
2019-08-02 00:29:59 -0500

    Test download request with fake fiel - WIP

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace PhpCfdi\SatWsDescargaMasiva\Tests\Unit;
 
+use PhpCfdi\SatWsDescargaMasiva\DateTime;
+use PhpCfdi\SatWsDescargaMasiva\DateTimePeriod;
+use PhpCfdi\SatWsDescargaMasiva\DownloadRequestQuery;
+use PhpCfdi\SatWsDescargaMasiva\Enums\DownloadType;
+use PhpCfdi\SatWsDescargaMasiva\Enums\RequestType;
 use PhpCfdi\SatWsDescargaMasiva\Fiel;
 use PhpCfdi\SatWsDescargaMasiva\Service;
 use PhpCfdi\SatWsDescargaMasiva\Tests\GuzzleWebClient;
@@ -23,5 +28,22 @@ class ExampleTest extends TestCase
         $service = new Service($fiel, $webclient);
         $token = $service->authenticate();
         $this->assertTrue($token->isValid());
+    }
+
+    public function testDownloadRequestUsingFakeFiel(): void
+    {
+        $fiel = new Fiel(
+            $this->fileContents('fake-fiel/aaa010101aaa_FIEL_password.key.pem'),
+            $this->fileContents('fake-fiel/aaa010101aaa_FIEL.cer'),
+            trim($this->fileContents('fake-fiel/password.txt'))
+        );
+        $webclient = new GuzzleWebClient();
+        $dateTimePeriod = new DateTimePeriod(new DateTime('2019-01-01 00:00:00'), new DateTime('2019-01-01 00:04:00'));
+        $rfc = 'aaa010101aaa';
+        $downloadRequestQuery = new DownloadRequestQuery($dateTimePeriod, $rfc, DownloadType::received(), RequestType::cfdi());
+
+        $service = new Service($fiel, $webclient);
+        $downloadRequestResult = $service->downloadRequest($downloadRequestQuery);
+        $this->assertTrue($downloadRequestResult->isAccepted());
     }
 }
