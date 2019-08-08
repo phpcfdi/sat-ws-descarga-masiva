1cfe9239294c0472fbb4a24910396a8b60411eee
Cesar Aguilera <cesargnu29@gmail.com>
2019-08-08 00:34:56 -0500

    Create download result

---
+++ src/Services/Download/DownloadResult.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpCfdi\SatWsDescargaMasiva\Services\Download;
+
+class DownloadResult
+{
+    /**
+     * @var int
+     */
+    private $statusCode;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @var string
+     */
+    private $package;
+
+    public function __construct(int $statusCode, string $message, string $package)
+    {
+        $this->statusCode = $statusCode;
+        $this->message = $message;
+        $this->package = $package;
+    }
+
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function getPackage(): string
+    {
+        return $this->package;
+    }
+
+    public function isAccepted(): bool
+    {
+        return 5000 === $this->getStatusCode();
+    }
+}
