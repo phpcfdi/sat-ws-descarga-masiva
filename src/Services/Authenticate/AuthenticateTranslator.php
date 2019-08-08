316dec90c28400ce5da2add7012e7790f7f81098
Carlos C Soto <eclipxe13@gmail.com>
2019-08-08 14:03:40 -0500

    Simplify AuthenticateTranslator (use URI)

--- src/Services/Authenticate/AuthenticateTranslator.php
+++ src/Services/Authenticate/AuthenticateTranslator.php
@@ -8,12 +8,14 @@ use PhpCfdi\SatWsDescargaMasiva\Shared\DateTime;
 use PhpCfdi\SatWsDescargaMasiva\Shared\Fiel;
 use PhpCfdi\SatWsDescargaMasiva\Shared\Helpers;
 use PhpCfdi\SatWsDescargaMasiva\Shared\InteractsXmlTrait;
+use PhpCfdi\SatWsDescargaMasiva\Shared\SignXmlHelpersTrait;
 use PhpCfdi\SatWsDescargaMasiva\Shared\Token;
 
 /** @internal */
 class AuthenticateTranslator
 {
     use InteractsXmlTrait;
+    use SignXmlHelpersTrait;
 
     public function createTokenFromSoapResponse(string $content): Token
     {
@@ -44,63 +46,45 @@ class AuthenticateTranslator
 </u:Timestamp>
 EOT
         );
-        $digested = base64_encode(sha1(str_replace(PHP_EOL, '', $toDigest), true));
-
-        $toSign = $this->nospaces(
-            <<<EOT
-<SignedInfo xmlns="http://www.w3.org/2000/09/xmldsig#">
-    <CanonicalizationMethod Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"></CanonicalizationMethod>
-    <SignatureMethod Algorithm="http://www.w3.org/2000/09/xmldsig#rsa-sha1"></SignatureMethod>
-    <Reference URI="#_0">
-        <Transforms>
-            <Transform Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"></Transform>
-        </Transforms>
-        <DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1"></DigestMethod>
-        <DigestValue>${digested}</DigestValue>
-    </Reference>
-</SignedInfo>
-EOT
-        );
-        $signed = base64_encode($fiel->sign($toSign, OPENSSL_ALGO_SHA1));
-
+        $digested = base64_encode(sha1($toDigest, true));
+        $signedInfoData = $this->createSignedInfoCanonicalExclusive($digested, '#_0');
+        $signed = base64_encode($fiel->sign($signedInfoData, OPENSSL_ALGO_SHA1));
+        $keyInfoData = $this->createKeyInfoSecurityToken($uuid);
+        $signatureData = $this->createSignatureData($signedInfoData, $signed, $keyInfoData);
         $certificate = Helpers::cleanPemContents($fiel->getCertificatePemContents());
 
         $xml = <<<EOT
-<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"><s:Header><o:Security s:mustUnderstand="1" xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
-    <u:Timestamp u:Id="_0">
-        <u:Created>${created}</u:Created>
-        <u:Expires>${expires}</u:Expires>
-    </u:Timestamp>
-        <o:BinarySecurityToken u:Id="${uuid}" ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3" EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary">
-            ${certificate}
-        </o:BinarySecurityToken>
-        <Signature xmlns="http://www.w3.org/2000/09/xmldsig#">
-            <SignedInfo>
-                <CanonicalizationMethod Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/>
-                <SignatureMethod Algorithm="http://www.w3.org/2000/09/xmldsig#rsa-sha1"/>
-                <Reference URI="#_0">
-                    <Transforms>
-                        <Transform Algorithm="http://www.w3.org/2001/10/xml-exc-c14n#"/>
-                    </Transforms>
-                    <DigestMethod Algorithm="http://www.w3.org/2000/09/xmldsig#sha1"/>
-                    <DigestValue>${digested}</DigestValue>
-                </Reference>
-            </SignedInfo>
-            <SignatureValue>${signed}</SignatureValue>
-            <KeyInfo>
-                <o:SecurityTokenReference>
-                    <o:Reference ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3" URI="#${uuid}"/>
-                </o:SecurityTokenReference>
-            </KeyInfo>
-        </Signature>
-            </o:Security>
-        </s:Header>
-        <s:Body>
-            <Autentica xmlns="http://DescargaMasivaTerceros.gob.mx"/>
-        </s:Body>
-    </s:Envelope>
+<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" xmlns:u="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+    <s:Header>
+        <o:Security xmlns:o="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" s:mustUnderstand="1">
+            <u:Timestamp u:Id="_0">
+                <u:Created>${created}</u:Created>
+                <u:Expires>${expires}</u:Expires>
+            </u:Timestamp>
+            <o:BinarySecurityToken u:Id="${uuid}" ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3" EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary">
+                ${certificate}
+            </o:BinarySecurityToken>
+            ${signatureData}
+        </o:Security>
+    </s:Header>
+    <s:Body>
+        <Autentica xmlns="http://DescargaMasivaTerceros.gob.mx"/>
+    </s:Body>
+</s:Envelope>
 EOT;
 
         return $this->nospaces($xml);
+    }
+
+    public function createKeyInfoSecurityToken(string $uuid): string
+    {
+        $xml = <<<EOT
+<KeyInfo>
+    <o:SecurityTokenReference>
+        <o:Reference URI="#${uuid}" ValueType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-x509-token-profile-1.0#X509v3"/>
+    </o:SecurityTokenReference>
+</KeyInfo>
+EOT;
+        return $xml;
     }
 }
