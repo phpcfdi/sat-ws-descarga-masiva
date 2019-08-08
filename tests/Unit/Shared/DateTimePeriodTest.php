c1706aeed10c490bb1832a87774c30165f7726c4
Carlos C Soto <eclipxe13@gmail.com>
2019-08-07 21:01:12 -0500

    code-style

--- tests/Unit/Shared/DateTimePeriodTest.php
+++ tests/Unit/Shared/DateTimePeriodTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PhpCfdi\SatWsDescargaMasiva\Tests\Unit\Shared;
 
+use InvalidArgumentException;
 use PhpCfdi\SatWsDescargaMasiva\Shared\DateTime;
 use PhpCfdi\SatWsDescargaMasiva\Shared\DateTimePeriod;
 use PhpCfdi\SatWsDescargaMasiva\Tests\TestCase;
@@ -25,7 +26,7 @@ class DateTimePeriodTest extends TestCase
         $start = new DateTime('2019-01-01 00:00:59');
         $end = new DateTime('2019-01-01 00:00:55');
 
-        $this->expectException(\InvalidArgumentException::class);
+        $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('The final date must be greater than the initial date');
         new DateTimePeriod($start, $end);
     }
