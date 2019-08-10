8faf38a084e6bb5b177e3b64374b7b1a4ec6b97e
Carlos C Soto <eclipxe13@gmail.com>
2019-08-10 02:39:26 -0500

    unset variable instead of allocate empty string (thanks scrutinizer)
    This reverts commit f10451b8adc798056e8fb528dc934d9fba5c1118.

--- src/PackageReader/AbstractPackageReader.php
+++ src/PackageReader/AbstractPackageReader.php
@@ -107,7 +107,7 @@ abstract class AbstractPackageReader implements Countable
 
             $contents = $this->zip->getFromName($filename);
             if (false === $contents || ! $this->filterContents($contents)) {
-                $contents = '';
+                unset($contents); // release memory as it was filtered
                 continue; // did not pass the filename filter
             }
 
