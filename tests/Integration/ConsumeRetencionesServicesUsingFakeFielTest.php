100fa6528e73fbf43ee5b737eef8cb152e978a35
Carlos C Soto <eclipxe13@gmail.com>
2020-10-19 16:02:07 -0500

    Testing: SSL at clouda.sat.gob.mx is working fine

--- tests/Integration/ConsumeRetencionesServicesUsingFakeFielTest.php
+++ tests/Integration/ConsumeRetencionesServicesUsingFakeFielTest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace PhpCfdi\SatWsDescargaMasiva\Tests\Integration;
 
-use GuzzleHttp\Client as GuzzleHttpClient;
-use GuzzleHttp\RequestOptions;
 use PhpCfdi\SatWsDescargaMasiva\Service;
 use PhpCfdi\SatWsDescargaMasiva\Shared\ServiceEndpoints;
 use PhpCfdi\SatWsDescargaMasiva\WebClient\GuzzleWebClient;
@@ -15,11 +13,7 @@ final class ConsumeRetencionesServicesUsingFakeFielTest extends ConsumeServiceTe
     protected function createService(): Service
     {
         $requestBuilder = $this->createFielRequestBuilderUsingTestingFiles();
-        // 2020-10-16 The server https://cfdidescargamasivasolicitud.clouda.sat.gob.mx/ has invalid certificate
-        // There is not a problem for testing but do not execute use insecure connections with production data
-        // 2020-10-17 The same problem now exists only on https://cfdidescargamasiva.clouda.sat.gob.mx/
-        $customGuzzleHttpClient = new GuzzleHttpClient([RequestOptions::VERIFY => false]);
-        $webclient = new GuzzleWebClient($customGuzzleHttpClient);
+        $webclient = new GuzzleWebClient();
         return new Service($requestBuilder, $webclient, null, ServiceEndpoints::retenciones());
     }
 }
